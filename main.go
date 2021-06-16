2872cad8352d67bc7925610788403db4b162eb67
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2021-06-16 10:38:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World from Prod")
+        fmt.Fprintf(w, "Hello World from Go Application from Prod\n")
     })
 
     http.ListenAndServe(":9000", nil)
